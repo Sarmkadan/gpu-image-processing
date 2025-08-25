5262eced00750c8fd48e091d9285ae5662bd8391
Vladyslav Zaiets <rutova2@gmail.com>
2025-08-25 12:54:36 +0300

    feat(services): a couple of BathcProcessingService extensions

--- tests/gpu-image-processing.Tests/Services/BatchProcessingServiceTestsExtensions.cs
+++ tests/gpu-image-processing.Tests/Services/BatchProcessingServiceTestsExtensions.cs
@@ -17,11 +17,15 @@ public static class BatchProcessingServiceTestsExtensions
     /// <param name="imageCount">Number of images to include in the batch</param>
     /// <param name="filterCount">Number of filters to include in the batch</param>
     /// <returns>An ImageBatch with the specified configuration</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if imageCount or filterCount is negative</exception>
     public static ImageBatch CreateValidBatch(
         this BatchProcessingServiceTests _,
         int imageCount = 3,
         int filterCount = 2)
     {
+        ArgumentOutOfRangeException.ThrowIfNegative(imageCount);
+        ArgumentOutOfRangeException.ThrowIfNegative(filterCount);
+
         var batch = new ImageBatch
         {
             Name = "Extension Test Batch",
@@ -49,25 +53,31 @@ public static class BatchProcessingServiceTestsExtensions
     /// <param name="expectedProcessed">Expected number of processed images</param>
     /// <param name="expectedFailed">Expected number of failed images</param>
     /// <param name="expectedTotal">Expected total number of images</param>
+    /// <exception cref="ArgumentNullException">Thrown if batch is null</exception>
     public static void ShouldHaveProcessingStats(
         this ImageBatch batch,
         int expectedProcessed,
         int expectedFailed,
         int expectedTotal)
     {
+        ArgumentNullException.ThrowIfNull(batch);
+        ArgumentOutOfRangeException.ThrowIfNegative(expectedTotal);
+
         batch.ProcessedImages.Should().Be(expectedProcessed, "because {0} images should have been processed", expectedProcessed);
         batch.FailedImages.Should().Be(expectedFailed, "because {0} images should have failed", expectedFailed);
         batch.TotalImages.Should().Be(expectedTotal, "because total should be {0}", expectedTotal);
 
         var successRate = batch.GetSuccessRate();
-        if (expectedFailed == 0)
+
+        switch (expectedFailed)
         {
-            successRate.Should().Be(1.0, "because all images should have succeeded");
-        }
-        else
-        {
-            var expectedRate = (double)expectedProcessed / expectedTotal;
-            successRate.Should().BeApproximately(expectedRate, precision: 0.01);
+            case 0:
+                successRate.Should().Be(1.0, "because all images should have succeeded");
+                break;
+            default:
+                var expectedRate = (double)expectedProcessed / expectedTotal;
+                successRate.Should().BeApproximately(expectedRate, precision: 0.01);
+                break;
         }
     }
 
@@ -75,8 +85,11 @@ public static class BatchProcessingServiceTestsExtensions
     /// Asserts that a batch has completed processing.
     /// </summary>
     /// <param name="batch">The batch to assert</param>
+    /// <exception cref="ArgumentNullException">Thrown if batch is null</exception>
     public static void ShouldBeCompleted(this ImageBatch batch)
     {
+        ArgumentNullException.ThrowIfNull(batch);
+
         batch.Status.Should().Be(ProcessingStatus.Completed);
         batch.ProcessedImages.Should().BeGreaterThan(0);
         batch.FailedImages.Should().Be(0);
@@ -88,8 +101,14 @@ public static class BatchProcessingServiceTestsExtensions
     /// <param name="batch">The batch to assert</param>
     /// <param name="expectedProcessed">Expected number of processed images</param>
     /// <param name="expectedFailed">Expected number of failed images</param>
-    public static void ShouldHaveProcessedAndFailedCount(this ImageBatch batch, int expectedProcessed, int expectedFailed)
+    /// <exception cref="ArgumentNullException">Thrown if batch is null</exception>
+    public static void ShouldHaveProcessedAndFailedCount(
+        this ImageBatch batch,
+        int expectedProcessed,
+        int expectedFailed)
     {
+        ArgumentNullException.ThrowIfNull(batch);
+
         batch.ProcessedImages.Should().Be(expectedProcessed);
         batch.FailedImages.Should().Be(expectedFailed);
     }
