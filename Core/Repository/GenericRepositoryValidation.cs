d4d7b11cd69b833c3f1775d78b2cf22ea96e3b1c
Vladyslav Zaiets <rutova2@gmail.com>
2025-12-15 06:52:16 +0200

    feat(repository): cleanup

--- Core/Repository/GenericRepositoryValidation.cs
+++ Core/Repository/GenericRepositoryValidation.cs
@@ -13,14 +13,15 @@ namespace GpuImageProcessing.Core.Repository
     /// <summary>
     /// Validation helpers for <see cref="GenericRepository{T}"/>
     /// </summary>
-    public static class GenericRepositoryValidation<T> where T : class
+    public static class GenericRepositoryValidation
     {
         /// <summary>
         /// Validates a <see cref="GenericRepository{T}"/> instance
         /// </summary>
+        /// <typeparam name="T">The type of entity</typeparam>
         /// <param name="value">The instance to validate</param>
         /// <returns>A list of human-readable problems</returns>
-        public static IReadOnlyList<string> Validate(this GenericRepository<T>? value)
+        public static IReadOnlyList<string> Validate<T>(this GenericRepository<T>? value) where T : class
         {
             ArgumentNullException.ThrowIfNull(value);
 
@@ -44,16 +45,18 @@ namespace GpuImageProcessing.Core.Repository
         /// <summary>
         /// Checks if a <see cref="GenericRepository{T}"/> instance is valid
         /// </summary>
+        /// <typeparam name="T">The type of entity</typeparam>
         /// <param name="value">The instance to check</param>
         /// <returns>True if the instance is valid; otherwise, false</returns>
-        public static bool IsValid(this GenericRepository<T>? value) => Validate(value).Count == 0;
+        public static bool IsValid<T>(this GenericRepository<T>? value) where T : class => Validate(value).Count == 0;
 
         /// <summary>
         /// Ensures a <see cref="GenericRepository{T}"/> instance is valid
         /// </summary>
+        /// <typeparam name="T">The type of entity</typeparam>
         /// <param name="value">The instance to ensure</param>
         /// <exception cref="ArgumentException">If the instance is not valid</exception>
-        public static void EnsureValid(this GenericRepository<T>? value)
+        public static void EnsureValid<T>(this GenericRepository<T>? value) where T : class
         {
             var problems = Validate(value);
             if (problems.Count > 0)
