91906b1a6e9362e0bf6226fcb2db65044dbacbb1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-10 13:30:18 +0000

    feat(services): BatchProcessingServiceTests convenience extensions

---
+++ tests/gpu-image-processing.Tests/Services/BatchProcessingServiceTestsExtensions.cs
@@ -0,0 +1,96 @@
+#nullable enable
+
+using FluentAssertions;
+using GpuImageProcessing.Domain;
+using GpuImageProcessing.Services;
+using GpuImageProcessing.Core;
+using Xunit;
+
+namespace GpuImageProcessing.Tests.Services;
+
+public static class BatchProcessingServiceTestsExtensions
+{
+    /// <summary>
+    /// Creates a valid batch with images and filters.
+    /// </summary>
+    /// <param name="_">The BatchProcessingServiceTests instance (unused)</param>
+    /// <param name="imageCount">Number of images to include in the batch</param>
+    /// <param name="filterCount">Number of filters to include in the batch</param>
+    /// <returns>An ImageBatch with the specified configuration</returns>
+    public static ImageBatch CreateValidBatch(
+        this BatchProcessingServiceTests _,
+        int imageCount = 3,
+        int filterCount = 2)
+    {
+        var batch = new ImageBatch
+        {
+            Name = "Extension Test Batch",
+            OutputDirectory = "/output/extensions",
+            TotalImages = imageCount
+        };
+
+        for (int i = 0; i < imageCount; i++)
+        {
+            batch.AddImage(Guid.NewGuid());
+        }
+
+        for (int i = 0; i < filterCount; i++)
+        {
+            batch.AddFilter(Guid.NewGuid());
+        }
+
+        return batch;
+    }
+
+    /// <summary>
+    /// Asserts that a batch has the expected processing statistics.
+    /// </summary>
+    /// <param name="batch">The batch to assert</param>
+    /// <param name="expectedProcessed">Expected number of processed images</param>
+    /// <param name="expectedFailed">Expected number of failed images</param>
+    /// <param name="expectedTotal">Expected total number of images</param>
+    public static void ShouldHaveProcessingStats(
+        this ImageBatch batch,
+        int expectedProcessed,
+        int expectedFailed,
+        int expectedTotal)
+    {
+        batch.ProcessedImages.Should().Be(expectedProcessed, "because {0} images should have been processed", expectedProcessed);
+        batch.FailedImages.Should().Be(expectedFailed, "because {0} images should have failed", expectedFailed);
+        batch.TotalImages.Should().Be(expectedTotal, "because total should be {0}", expectedTotal);
+
+        var successRate = batch.GetSuccessRate();
+        if (expectedFailed == 0)
+        {
+            successRate.Should().Be(1.0, "because all images should have succeeded");
+        }
+        else
+        {
+            var expectedRate = (double)expectedProcessed / expectedTotal;
+            successRate.Should().BeApproximately(expectedRate, precision: 0.01);
+        }
+    }
+
+    /// <summary>
+    /// Asserts that a batch has completed processing.
+    /// </summary>
+    /// <param name="batch">The batch to assert</param>
+    public static void ShouldBeCompleted(this ImageBatch batch)
+    {
+        batch.Status.Should().Be(ProcessingStatus.Completed);
+        batch.ProcessedImages.Should().BeGreaterThan(0);
+        batch.FailedImages.Should().Be(0);
+    }
+
+    /// <summary>
+    /// Asserts that a batch has the expected number of processed and failed images.
+    /// </summary>
+    /// <param name="batch">The batch to assert</param>
+    /// <param name="expectedProcessed">Expected number of processed images</param>
+    /// <param name="expectedFailed">Expected number of failed images</param>
+    public static void ShouldHaveProcessedAndFailedCount(this ImageBatch batch, int expectedProcessed, int expectedFailed)
+    {
+        batch.ProcessedImages.Should().Be(expectedProcessed);
+        batch.FailedImages.Should().Be(expectedFailed);
+    }
+}
