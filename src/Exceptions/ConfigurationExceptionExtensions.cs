6c9aa6b462357740b82193aef16f8633a2710207
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-15 08:30:56 +0000

    fix(exceptions): resolve build errors in ConfigurationExceptionExtensions

---
+++ src/Exceptions/ConfigurationExceptionExtensions.cs
@@ -0,0 +1,54 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace GpuImageProcessing.Exceptions;
+
+/// <summary>
+/// Extensions for <see cref="ConfigurationException"/>.
+/// </summary>
+public static class ConfigurationExceptionExtensions
+{
+    /// <summary>
+    /// Determines if a <see cref="ConfigurationException"/> is due to an invalid configuration key.
+    /// </summary>
+    /// <param name="exception">The <see cref="ConfigurationException"/> to check.</param>
+    /// <returns><c>true</c> if the exception is due to an invalid configuration key; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
+    public static bool IsInvalidKey(this ConfigurationException exception)
+    {
+        ArgumentNullException.ThrowIfNull(exception);
+        return !string.IsNullOrEmpty(exception.ConfigurationKey) && string.IsNullOrEmpty(exception.ConfigurationValue);
+    }
+
+    /// <summary>
+    /// Determines if a <see cref="ConfigurationException"/> is due to an invalid configuration value.
+    /// </summary>
+    /// <param name="exception">The <see cref="ConfigurationException"/> to check.</param>
+    /// <returns><c>true</c> if the exception is due to an invalid configuration value; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
+    public static bool IsInvalidValue(this ConfigurationException exception)
+    {
+        ArgumentNullException.ThrowIfNull(exception);
+        return !string.IsNullOrEmpty(exception.ConfigurationValue) && string.IsNullOrEmpty(exception.ConfigurationKey);
+    }
+
+    /// <summary>
+    /// Gets a formatted string that describes the configuration error.
+    /// </summary>
+    /// <param name="exception">The <see cref="ConfigurationException"/> to format.</param>
+    /// <returns>A formatted string that describes the configuration error.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <c>null</c>.</exception>
+    public static string GetFormattedError(this ConfigurationException exception)
+    {
+        ArgumentNullException.ThrowIfNull(exception);
+        var errorMessage = exception.ToString();
+        if (exception.IsInvalidKey())
+            errorMessage += "\nError: Invalid configuration key.";
+        else if (exception.IsInvalidValue())
+            errorMessage += "\nError: Invalid configuration value.";
+        return errorMessage;
+    }
+}
