b87843c6e68994eb5c27e2139497793beb8a7753
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in TransformServiceValidation

--- Core/Services/TransformServiceValidation.cs
+++ Core/Services/TransformServiceValidation.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace GpuImageProcessing.Core.Services
 {
     /// <summary>
-    /// Validation helpers for <see cref="TransformService"/>.
+    /// Provides validation extensions for <see cref="TransformService"/> instances.
     /// </summary>
     public static class TransformServiceValidation
     {
@@ -15,16 +15,18 @@ namespace GpuImageProcessing.Core.Services
         /// </summary>
         /// <param name="value">The service instance to validate.</param>
         /// <returns>
-        /// A read‑only list of validation problem descriptions.
+        /// A read-only list of validation problem descriptions.
         /// The list is empty when the instance is considered valid.
         /// </returns>
+        /// <remarks>
+        /// The only observable state of the service is its existence;
+        /// all other members are methods that do not expose mutable public properties.
+        /// </remarks>
         public static IReadOnlyList<string> Validate(this TransformService? value)
         {
-            // The only observable state of the service is its existence;
-            // all other members are methods that do not expose mutable public properties.
-            return value is null
-                ? new[] { "TransformService instance is null." }
-                : Array.Empty<string>();
+            ArgumentNullException.ThrowIfNull(value, nameof(value));
+
+            return Array.Empty<string>();
         }
 
         /// <summary>
@@ -32,21 +34,30 @@ namespace GpuImageProcessing.Core.Services
         /// </summary>
         /// <param name="value">The service instance to check.</param>
         /// <returns><c>true</c> if the instance has no validation problems; otherwise <c>false</c>.</returns>
-        public static bool IsValid(this TransformService? value) => !value.Validate().Any();
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="value"/> is <c>null</c>.
+        /// </exception>
+        public static bool IsValid(this TransformService? value) => value?.Validate().Count == 0;
 
         /// <summary>
         /// Ensures that the <see cref="TransformService"/> instance is valid.
         /// </summary>
         /// <param name="value">The service instance to validate.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="value"/> is <c>null</c>.
+        /// </exception>
         /// <exception cref="ArgumentException">
         /// Thrown when one or more validation problems are found.
-        /// The exception message contains a semicolon‑separated list of problems.
+        /// The exception message contains a semicolon-separated list of problems.
         /// </exception>
         public static void EnsureValid(this TransformService? value)
         {
-            var problems = value.Validate();
-            if (problems.Any())
+            var problems = value?.Validate() ?? throw new ArgumentNullException(nameof(value));
+
+            if (problems.Count > 0)
+            {
                 throw new ArgumentException(string.Join("; ", problems));
+            }
         }
     }
 }
