23cd6e995bc4ab0a2db1a39ef063964a8e96320f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    cleanup

--- Core/Repository/GenericRepositoryExtensions.cs
+++ Core/Repository/GenericRepositoryExtensions.cs
@@ -5,7 +5,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -24,7 +23,7 @@ namespace GpuImageProcessing.Core.Repository
         /// <param name="predicate">The condition to apply.</param>
         /// <returns>An <see cref="IReadOnlyList{T}"/> of entities that match the predicate.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="repository"/> or <paramref name="predicate"/> is null.</exception>
-        public static async Task<IReadOnlyList<T>> FindAllAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate) 
+        public static async Task<IReadOnlyList<T>> FindAllAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate)
             where T : class
         {
             ArgumentNullException.ThrowIfNull(repository);
@@ -42,14 +41,14 @@ namespace GpuImageProcessing.Core.Repository
         /// <param name="predicate">The condition to apply.</param>
         /// <returns><see langword="true"/> if any entities match the predicate; otherwise, <see langword="false"/>.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="repository"/> or <paramref name="predicate"/> is null.</exception>
-        public static async Task<bool> AnyAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate) 
+        public static async Task<bool> AnyAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate)
             where T : class
         {
             ArgumentNullException.ThrowIfNull(repository);
             ArgumentNullException.ThrowIfNull(predicate);
 
-            var entities = await repository.FindAllAsync(predicate);
-            return entities.Count > 0;
+            var allEntities = await repository.GetAllAsync();
+            return allEntities.Any(predicate);
         }
 
         /// <summary>
@@ -60,14 +59,14 @@ namespace GpuImageProcessing.Core.Repository
         /// <param name="predicate">The condition to apply.</param>
         /// <returns>The first matching entity, or <see langword="default"/> if none are found.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="repository"/> or <paramref name="predicate"/> is null.</exception>
-        public static async Task<T?> FirstOrDefaultAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate) 
+        public static async Task<T?> FirstOrDefaultAsync<T>(this GenericRepository<T> repository, Func<T, bool> predicate)
             where T : class
         {
             ArgumentNullException.ThrowIfNull(repository);
             ArgumentNullException.ThrowIfNull(predicate);
 
-            var entities = await repository.FindAllAsync(predicate);
-            return entities.FirstOrDefault();
+            var allEntities = await repository.GetAllAsync();
+            return allEntities.FirstOrDefault(predicate);
         }
     }
 }
