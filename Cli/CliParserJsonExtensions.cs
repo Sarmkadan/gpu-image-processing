5cde2ec1727072a56b5afca1e1d275b76ddbc9c1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tighten guard clauses in CliParserJsonExtensions

--- Cli/CliParserJsonExtensions.cs
+++ Cli/CliParserJsonExtensions.cs
@@ -24,9 +24,9 @@ namespace GpuImageProcessing.Cli
         };
 
         /// <summary>
-        /// Serializes the <see cref="CliParser"/> instance to a JSON string.
+        /// Serializes the <see cref="CliParser"/> instance to a JSON string using camelCase property naming.
         /// </summary>
-        /// <param name="value">The CLI parser instance to serialize.</param>
+        /// <param name="value">The CLI parser instance to serialize. Cannot be null.</param>
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
         /// <returns>A JSON string representation of the CLI parser.</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
@@ -47,7 +47,7 @@ namespace GpuImageProcessing.Cli
         /// <summary>
         /// Deserializes a JSON string to a <see cref="CliParser"/> instance.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="json">The JSON string to deserialize. Can be null or empty.</param>
         /// <returns>The deserialized <see cref="CliParser"/> instance, or null if the JSON is null or empty.</returns>
         /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
         /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or whitespace.</exception>
@@ -64,7 +64,7 @@ namespace GpuImageProcessing.Cli
         /// <summary>
         /// Attempts to deserialize a JSON string to a <see cref="CliParser"/> instance.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="json">The JSON string to deserialize. Can be null or empty.</param>
         /// <param name="value">Receives the deserialized instance if successful.</param>
         /// <returns>True if deserialization succeeded; otherwise, false.</returns>
         /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or whitespace.</exception>
