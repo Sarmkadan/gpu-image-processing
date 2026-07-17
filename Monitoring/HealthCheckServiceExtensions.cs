2b7b075cbc7ad74a457ab3883bb58d2063299e9b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Small refactor - HealthCheckServiceExtensions

--- Monitoring/HealthCheckServiceExtensions.cs
+++ Monitoring/HealthCheckServiceExtensions.cs
@@ -1,23 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GpuImageProcessing.Monitoring
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="HealthCheckService"/> to simplify health check registration and monitoring.
+    /// </summary>
     public static class HealthCheckServiceExtensions
     {
         /// <summary>
         /// Registers a health check component with a specified name and implementation.
         /// </summary>
-        /// <param name="healthCheckService">The health check service instance.</param>
-        /// <param name="name">The name of the health check component.</param>
-        /// <param name="healthCheck">The implementation of the health check component.</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="healthCheckService"/> or <paramref name="name"/> is null.</exception>
+        /// <param name="healthCheckService">The health check service instance. Cannot be null.</param>
+        /// <param name="name">The name of the health check component. Cannot be null.</param>
+        /// <param name="healthCheck">The implementation of the health check component. Cannot be null.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="healthCheckService"/>, <paramref name="name"/>, or <paramref name="healthCheck"/> is null.</exception>
         public static void RegisterHealthCheck(this HealthCheckService healthCheckService, string name, IHealthCheck healthCheck)
         {
             ArgumentNullException.ThrowIfNull(healthCheckService);
             ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(healthCheck);
 
             healthCheckService.RegisterHealthCheck(name, healthCheck);
         }
@@ -25,29 +27,29 @@ namespace GpuImageProcessing.Monitoring
         /// <summary>
         /// Checks the health of all registered components and returns the overall health status.
         /// </summary>
-        /// <param name="healthCheckService">The health check service instance.</param>
+        /// <param name="healthCheckService">The health check service instance. Cannot be null.</param>
         /// <returns>The overall health status of all registered components.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="healthCheckService"/> is null.</exception>
-        public static async Task<HealthCheckResult> CheckHealthAsync(this HealthCheckService healthCheckService)
+        public static ValueTask<HealthCheckResult> CheckHealthAsync(this HealthCheckService healthCheckService)
         {
             ArgumentNullException.ThrowIfNull(healthCheckService);
 
-            return await healthCheckService.CheckHealthAsync();
+            return new ValueTask<HealthCheckResult>(healthCheckService.CheckHealthAsync());
         }
 
         /// <summary>
         /// Checks the health of a specific component and returns its health status.
         /// </summary>
-        /// <param name="healthCheckService">The health check service instance.</param>
-        /// <param name="componentName">The name of the component to check.</param>
+        /// <param name="healthCheckService">The health check service instance. Cannot be null.</param>
+        /// <param name="componentName">The name of the component to check. Cannot be null or empty.</param>
         /// <returns>The health status of the specified component.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="healthCheckService"/> or <paramref name="componentName"/> is null.</exception>
-        public static async Task<ComponentHealth> CheckComponentAsync(this HealthCheckService healthCheckService, string componentName)
+        public static ValueTask<ComponentHealth> CheckComponentAsync(this HealthCheckService healthCheckService, string componentName)
         {
             ArgumentNullException.ThrowIfNull(healthCheckService);
             ArgumentNullException.ThrowIfNull(componentName);
 
-            return await healthCheckService.CheckComponentAsync(componentName);
+            return new ValueTask<ComponentHealth>(healthCheckService.CheckComponentAsync(componentName));
         }
     }
 }
