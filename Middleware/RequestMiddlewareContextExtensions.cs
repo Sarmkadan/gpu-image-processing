080c9cc9322fb63bcf16efc7e432916f7dc370d2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Polish RequestMiddlewareContextExtensions
    also touched a couple of call sites.

--- Middleware/RequestMiddlewareContextExtensions.cs
+++ Middleware/RequestMiddlewareContextExtensions.cs
@@ -32,7 +32,8 @@ namespace GpuImageProcessing.Middleware
         {
             ArgumentNullException.ThrowIfNull(context);
             ArgumentException.ThrowIfNullOrEmpty(scope);
-            return context.Scopes.Contains(scope);
+
+            return context.Scopes?.Contains(scope) ?? false;
         }
 
         /// <summary>
@@ -41,13 +42,15 @@ namespace GpuImageProcessing.Middleware
         /// <param name="context">The <see cref="RequestMiddlewareContext"/> instance.</param>
         /// <param name="key">The metadata key.</param>
         /// <param name="value">The metadata value.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/> or <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/>, <paramref name="key"/>, or <paramref name="value"/> is null.</exception>
         /// <exception cref="ArgumentException">Thrown when <paramref name="key"/> is null or empty.</exception>
         public static void AddMetadata(this RequestMiddlewareContext context, string key, object value)
         {
             ArgumentNullException.ThrowIfNull(context);
             ArgumentException.ThrowIfNullOrEmpty(key);
             ArgumentNullException.ThrowIfNull(value);
+
+            context.Metadata ??= new Dictionary<string, object>();
             context.Metadata[key] = value;
         }
 
@@ -60,7 +63,8 @@ namespace GpuImageProcessing.Middleware
         public static IEnumerable<string> GetMetadataKeys(this RequestMiddlewareContext context)
         {
             ArgumentNullException.ThrowIfNull(context);
-            return context.Metadata.Keys;
+
+            return context.Metadata?.Keys ?? Enumerable.Empty<string>();
         }
     }
 }
