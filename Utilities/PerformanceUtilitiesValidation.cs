97e373395ffb63db9169d2c5215ded6399381386
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor(utilities): cleanup in PerformanceUtilitiesValidation
    found this while looking at something else.

--- Utilities/PerformanceUtilitiesValidation.cs
+++ Utilities/PerformanceUtilitiesValidation.cs
@@ -13,8 +13,8 @@ namespace GpuImageProcessing.Utilities
         /// Validates the <see cref="PerformanceTimer"/> instance.
         /// </summary>
         /// <param name="value">The timer instance to validate.</param>
-        /// <returns>A list of validation problems.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if value is null.</exception>
+        /// <returns>A list of validation problems. Returns empty list if timer is valid.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
         public static IReadOnlyList<string> Validate(this PerformanceTimer value)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -33,8 +33,8 @@ namespace GpuImageProcessing.Utilities
         /// Checks if the <see cref="PerformanceTimer"/> instance is valid.
         /// </summary>
         /// <param name="value">The timer instance to validate.</param>
-        /// <returns>True if the timer is valid, false otherwise.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if value is null.</exception>
+        /// <returns><see langword="true"/> if the timer is valid; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
         public static bool IsValid(this PerformanceTimer value)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -45,8 +45,8 @@ namespace GpuImageProcessing.Utilities
         /// Ensures the <see cref="PerformanceTimer"/> instance is valid.
         /// </summary>
         /// <param name="value">The timer instance to validate.</param>
-        /// <exception cref="ArgumentNullException">Thrown if value is null.</exception>
-        /// <exception cref="ArgumentException">Thrown if the timer is invalid.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">The timer has invalid elapsed time.</exception>
         public static void EnsureValid(this PerformanceTimer value)
         {
             ArgumentNullException.ThrowIfNull(value);
