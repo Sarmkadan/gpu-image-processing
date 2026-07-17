f60e38da733ca5162d541fd8e34cde09bd600f16
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor: small refactor - CommandHandlerJsonExtensions

--- Cli/CommandHandlerJsonExtensions.cs
+++ Cli/CommandHandlerJsonExtensions.cs
@@ -5,7 +5,6 @@
 // =============================================================================
 
 using System;
-using System.Collections.Generic;
 using System.Text.Json;
 
 namespace GpuImageProcessing.Cli
