6f0480c7c2837068f9217cbdcadf8bcc8a5fea3b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - ImageBatchExtensions

--- src/Domain/ImageBatchExtensions.cs
+++ src/Domain/ImageBatchExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 using GpuImageProcessing.Core;
 using GpuImageProcessing.Domain;
@@ -14,114 +14,120 @@ namespace GpuImageProcessing.Domain;
 /// </summary>
 public static class ImageBatchExtensions
 {
-    /// <summary>
-    /// Adds multiple images to the batch in a single operation.
-    /// </summary>
-    /// <param name="batch">The image batch to add images to.</param>
-    /// <param name="imageIds">The collection of image IDs to add.</param>
-    /// <returns>The number of images successfully added to the batch.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="imageIds"/> is null.</exception>
-    public static int AddImages(this ImageBatch batch, IEnumerable<Guid> imageIds)
-    {
-        ArgumentNullException.ThrowIfNull(batch);
-        ArgumentNullException.ThrowIfNull(imageIds);
+	/// <summary>
+	/// Adds multiple images to the batch in a single operation.
+	/// </summary>
+	/// <param name="batch">The image batch to add images to.</param>
+	/// <param name="imageIds">The collection of image IDs to add.</param>
+	/// <returns>The number of images successfully added to the batch.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="imageIds"/> is null.</exception>
+	public static int AddImages(this ImageBatch batch, IEnumerable<Guid> imageIds)
+	{
+		ArgumentNullException.ThrowIfNull(batch);
+		ArgumentNullException.ThrowIfNull(imageIds);
 
-        var addedCount = 0;
-        var maxBatchSize = AppConstants.Processing.MaxBatchSize;
-        var currentCount = batch.ImageIds.Count;
+		var addedCount = 0;
+		var maxBatchSize = AppConstants.Processing.MaxBatchSize;
+		var currentCount = batch.ImageIds.Count;
 
-        foreach (var imageId in imageIds)
-        {
-            if (batch.ImageIds.Contains(imageId))
-                continue;
+		foreach (var imageId in imageIds)
+		{
+			if (batch.ImageIds.Contains(imageId))
+				continue;
 
-            if (currentCount + addedCount >= maxBatchSize)
-                break;
+			if (currentCount + addedCount >= maxBatchSize)
+				break;
 
-            batch.ImageIds.Add(imageId);
-            addedCount++;
-        }
+			batch.ImageIds.Add(imageId);
+			addedCount++;
+		}
 
-        batch.TotalImages = batch.ImageIds.Count;
-        return addedCount;
-    }
+		batch.TotalImages = batch.ImageIds.Count;
+		return addedCount;
+	}
 
-    /// <summary>
-    /// Removes multiple images from the batch in a single operation.
-    /// </summary>
-    /// <param name="batch">The image batch to remove images from.</param>
-    /// <param name="imageIds">The collection of image IDs to remove.</param>
-    /// <returns>The number of images successfully removed from the batch.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="imageIds"/> is null.</exception>
-    public static int RemoveImages(this ImageBatch batch, IEnumerable<Guid> imageIds)
-    {
-        ArgumentNullException.ThrowIfNull(batch);
-        ArgumentNullException.ThrowIfNull(imageIds);
+	/// <summary>
+	/// Removes multiple images from the batch in a single operation.
+	/// </summary>
+	/// <param name="batch">The image batch to remove images from.</param>
+	/// <param name="imageIds">The collection of image IDs to remove.</param>
+	/// <returns>The number of images successfully removed from the batch.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="imageIds"/> is null.</exception>
+	public static int RemoveImages(this ImageBatch batch, IEnumerable<Guid> imageIds)
+	{
+		ArgumentNullException.ThrowIfNull(batch);
+		ArgumentNullException.ThrowIfNull(imageIds);
 
-        var removedCount = 0;
-        foreach (var imageId in imageIds)
-        {
-            if (batch.ImageIds.Remove(imageId))
-                removedCount++;
-        }
+		var removedCount = 0;
+		foreach (var imageId in imageIds)
+		{
+			if (batch.ImageIds.Remove(imageId))
+				removedCount++;
+		}
 
-        batch.TotalImages = batch.ImageIds.Count;
-        return removedCount;
-    }
+		batch.TotalImages = batch.ImageIds.Count;
+		return removedCount;
+	}
 
-    /// <summary>
-    /// Adds multiple filters to the batch processing pipeline in a single operation.
-    /// </summary>
-    /// <param name="batch">The image batch to add filters to.</param>
-    /// <param name="filterIds">The collection of filter IDs to add.</param>
-    /// <returns>The number of filters successfully added to the batch.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="filterIds"/> is null.</exception>
-    public static int AddFilters(this ImageBatch batch, IEnumerable<Guid> filterIds)
-    {
-        ArgumentNullException.ThrowIfNull(batch);
-        ArgumentNullException.ThrowIfNull(filterIds);
+	/// <summary>
+	/// Adds multiple filters to the batch processing pipeline in a single operation.
+	/// </summary>
+	/// <param name="batch">The image batch to add filters to.</param>
+	/// <param name="filterIds">The collection of filter IDs to add.</param>
+	/// <returns>The number of filters successfully added to the batch.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="batch"/> or <paramref name="filterIds"/> is null.</exception>
+	public static int AddFilters(this ImageBatch batch, IEnumerable<Guid> filterIds)
+	{
+		ArgumentNullException.ThrowIfNull(batch);
+		ArgumentNullException.ThrowIfNull(filterIds);
 
-        var addedCount = 0;
-        foreach (var filterId in filterIds)
-        {
-            if (batch.FilterIds.Contains(filterId))
-                continue;
+		var addedCount = 0;
+		var maxBatchSize = AppConstants.Processing.MaxBatchSize;
+		var currentCount = batch.FilterIds.Count;
 
-            batch.FilterIds.Add(filterId);
-            addedCount++;
-        }
+		foreach (var filterId in filterIds)
+		{
+			if (batch.FilterIds.Contains(filterId))
+				continue;
 
-        return addedCount;
-    }
+			if (currentCount + addedCount >= maxBatchSize)
+				break;
 
-    /// <summary>
-    /// Gets the batch completion status based on processing metrics.
-    /// </summary>
-    /// <param name="batch">The image batch to check.</param>
-    /// <returns>A <see cref="ProcessingStatus"/> value indicating the batch completion state.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="batch"/> is null.</exception>
-    public static ProcessingStatus GetCompletionStatus(this ImageBatch batch)
-    {
-        ArgumentNullException.ThrowIfNull(batch);
+			batch.FilterIds.Add(filterId);
+			addedCount++;
+		}
 
-        if (batch.Status == ProcessingStatus.Completed || batch.Status == ProcessingStatus.Failed)
-            return batch.Status;
+		return addedCount;
+	}
 
-        if (batch.Status == ProcessingStatus.Cancelled)
-            return ProcessingStatus.Cancelled;
+	/// <summary>
+	/// Gets the batch completion status based on processing metrics.
+	/// </summary>
+	/// <param name="batch">The image batch to check.</param>
+	/// <returns>A <see cref="ProcessingStatus"/> value indicating the batch completion state.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="batch"/> is null.</exception>
+	public static ProcessingStatus GetCompletionStatus(this ImageBatch batch)
+	{
+		ArgumentNullException.ThrowIfNull(batch);
 
-        if (batch.TotalImages == 0)
-            return ProcessingStatus.Pending;
+		switch (batch.Status)
+		{
+			case ProcessingStatus.Completed or ProcessingStatus.Failed:
+				return batch.Status;
+			case ProcessingStatus.Cancelled:
+				return ProcessingStatus.Cancelled;
+			case ProcessingStatus.Queued:
+				return ProcessingStatus.Queued;
+		}
 
-        var progress = batch.GetProgressPercentage();
-        var successRate = batch.GetSuccessRate();
+		if (batch.TotalImages == 0)
+			return ProcessingStatus.Pending;
 
-        if (progress >= 100.0)
-            return ProcessingStatus.Completed;
+		var progress = batch.GetProgressPercentage();
+		var successRate = batch.GetSuccessRate();
 
-        if (batch.Status == ProcessingStatus.Queued)
-            return ProcessingStatus.Queued;
-
-        return batch.Status;
-    }
+		return progress >= 100.0
+			? ProcessingStatus.Completed
+			: batch.Status;
+	}
 }
