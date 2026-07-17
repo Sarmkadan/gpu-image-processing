4925a60846f16c959c7f5fef1175fd6bbd32a8b3
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    polish BatchProcessingServiceJsonExtensions

--- src/Services/BatchProcessingServiceJsonExtensions.cs
+++ src/Services/BatchProcessingServiceJsonExtensions.cs
@@ -17,6 +17,7 @@ public static class BatchProcessingServiceJsonExtensions
         WriteIndented = false,
     };
 
+
     /// <summary>
     /// Serializes the <see cref="BatchProcessingService"/> instance to a JSON string.
     /// </summary>
@@ -52,6 +53,7 @@ public static class BatchProcessingServiceJsonExtensions
     /// <param name="value">Receives the deserialized service instance, or <see langword="null"/> if the JSON represents a null value.</param>
     /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
     public static bool TryFromJson(string json, out BatchProcessingService? value)
     {
         ArgumentNullException.ThrowIfNull(json);
@@ -69,11 +71,5 @@ public static class BatchProcessingServiceJsonExtensions
     }
 
     private static JsonSerializerOptions GetIndentedOptions()
-    {
-        var options = new JsonSerializerOptions(_jsonOptions)
-        {
-            WriteIndented = true,
-        };
-        return options;
-    }
+        => new JsonSerializerOptions(_jsonOptions) { WriteIndented = true };
 }
