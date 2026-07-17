2c3b75565189cb19e63b9ada107eb96a9250587b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Readability pass over ComputeShaderPipelineJsonExtensions

--- src/Pipeline/ComputeShaderPipelineJsonExtensions.cs
+++ src/Pipeline/ComputeShaderPipelineJsonExtensions.cs
@@ -46,9 +46,9 @@ public static class ComputeShaderPipelineJsonExtensions
     /// Deserializes a JSON string into a <see cref="ComputeShaderPipeline"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized pipeline instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    /// <returns>The deserialized pipeline instance, or <see langword="null"/> if <paramref name="json"/> is empty or whitespace.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>, empty, or consists only of whitespace.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized into a <see cref="ComputeShaderPipeline"/>.</exception>
     public static ComputeShaderPipeline? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -67,7 +67,7 @@ public static class ComputeShaderPipelineJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized instance if successful; otherwise, <see langword="null"/>.</param>
     /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>, empty, or consists only of whitespace.</exception>
     public static bool TryFromJson(string json, out ComputeShaderPipeline? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
