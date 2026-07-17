59c9d44906de5ea44b3cd6f99503f9ffc7e8c2fd
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Nits
    will circle back to tests later.

--- src/Domain/ProcessingResultExtensions.cs
+++ src/Domain/ProcessingResultExtensions.cs
@@ -14,11 +14,10 @@ public static class ProcessingResultExtensions
     /// </summary>
     /// <param name="result">The processing result.</param>
     /// <returns>An <see cref="IEnumerable{T}"/> of filter names.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="result"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="result"/> is null.</exception>
     public static IEnumerable<string> GetFilterNames(this ProcessingResult result)
     {
         ArgumentNullException.ThrowIfNull(result);
-
         return result.FiltersApplied.Select(f => f.FilterName);
     }
 
@@ -27,11 +26,10 @@ public static class ProcessingResultExtensions
     /// </summary>
     /// <param name="result">The processing result.</param>
     /// <returns>The total processing time.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="result"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="result"/> is null.</exception>
     public static TimeSpan GetDurationTimeSpan(this ProcessingResult result)
     {
         ArgumentNullException.ThrowIfNull(result);
-
         return TimeSpan.FromMilliseconds(result.ProcessingTimeMilliseconds);
     }
 
@@ -41,19 +39,16 @@ public static class ProcessingResultExtensions
     /// <typeparam name="T">The type of the metadata value.</typeparam>
     /// <param name="result">The processing result.</param>
     /// <param name="key">The metadata key.</param>
-    /// <returns>The metadata value, or default(T) if the key is not found or the value is of the wrong type.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="result"/> or <paramref name="key"/> is null.</exception>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="key"/> is empty.</exception>
+    /// <returns>The metadata value, or <see langword="default"/> if the key is not found or the value is of the wrong type.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="result"/> or <paramref name="key"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="key"/> is empty.</exception>
     public static T? GetMetadataValue<T>(this ProcessingResult result, string key)
     {
         ArgumentNullException.ThrowIfNull(result);
         ArgumentException.ThrowIfNullOrEmpty(key);
 
-        if (result.ResultMetadata.TryGetValue(key, out var value) && value is T typedValue)
-        {
-            return typedValue;
-        }
-
-        return default;
+        return result.ResultMetadata.TryGetValue(key, out var value) && value is T typedValue
+            ? typedValue
+            : default;
     }
 }
