ea7067a20c95d82096e1bb80f0e923fccb65350c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor: tidy up TelemetryServiceExtensions

--- Services/TelemetryServiceExtensions.cs
+++ Services/TelemetryServiceExtensions.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace GpuImageProcessing.Services
 {
@@ -15,18 +14,18 @@ namespace GpuImageProcessing.Services
         /// <param name="service">The telemetry service instance.</param>
         /// <param name="message">A descriptive error message.</param>
         /// <param name="exception">The exception to record.</param>
-        /// <exception cref="ArgumentNullException">Thrown when service or exception is null.</exception>
-        /// <exception cref="ArgumentException">Thrown when message is null or empty.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="service"/> or <paramref name="exception"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="message"/> is <see langword="null"/> or empty.</exception>
         public static void RecordError(this TelemetryService service, string message, Exception exception)
         {
             ArgumentNullException.ThrowIfNull(service);
             ArgumentException.ThrowIfNullOrEmpty(message);
             ArgumentNullException.ThrowIfNull(exception);
 
-            var properties = new Dictionary<string, object>
+            var properties = new Dictionary<string, object>(2)
             {
-                { "Message", message },
-                { "Exception", exception.ToString() }
+                ["Message"] = message,
+                ["Exception"] = exception.ToString()
             };
 
             service.RecordEvent("Error", properties, "error");
@@ -38,8 +37,8 @@ namespace GpuImageProcessing.Services
         /// <param name="service">The telemetry service instance.</param>
         /// <param name="operationName">The name of the operation to time.</param>
         /// <returns>An <see cref="IDisposable"/> token that stops the timer when disposed.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when service is null.</exception>
-        /// <exception cref="ArgumentException">Thrown when operationName is null or empty.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="service"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="operationName"/> is <see langword="null"/> or empty.</exception>
         public static IDisposable MeasureOperation(this TelemetryService service, string operationName)
         {
             ArgumentNullException.ThrowIfNull(service);
