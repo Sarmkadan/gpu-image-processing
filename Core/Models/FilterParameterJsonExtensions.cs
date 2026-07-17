c12dbdf9d01441c5f08450301ad530f00ac64cd4
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    dedupe some logic in FilterParameterJsonExtensions

--- Core/Models/FilterParameterJsonExtensions.cs
+++ Core/Models/FilterParameterJsonExtensions.cs
@@ -11,7 +11,7 @@ using System.Text.Json.Serialization;
 namespace GpuImageProcessing.Core.Models
 {
     /// <summary>
-    /// Provides JSON serialization and deserialization extensions for FilterParameter
+    /// Provides JSON serialization and deserialization extensions for <see cref="FilterParameter"/>.
     /// </summary>
     public static class FilterParameterJsonExtensions
     {
@@ -24,12 +24,12 @@ namespace GpuImageProcessing.Core.Models
         };
 
         /// <summary>
-        /// Serializes a FilterParameter to a JSON string
+        /// Serializes a FilterParameter to a JSON string.
         /// </summary>
-        /// <param name="value">The FilterParameter to serialize</param>
-        /// <param name="indented">Whether to format the JSON with indentation</param>
-        /// <returns>A JSON string representation of the FilterParameter</returns>
-        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        /// <param name="value">The FilterParameter to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation.</param>
+        /// <returns>A JSON string representation of the FilterParameter.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static string ToJson(this FilterParameter value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -42,11 +42,11 @@ namespace GpuImageProcessing.Core.Models
         }
 
         /// <summary>
-        /// Deserializes a FilterParameter from a JSON string
+        /// Deserializes a FilterParameter from a JSON string.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize</param>
-        /// <returns>The deserialized FilterParameter, or null if JSON is empty</returns>
-        /// <exception cref="JsonException">Thrown when JSON is invalid or cannot be deserialized</exception>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>The deserialized FilterParameter, or null if JSON is empty or whitespace.</returns>
+        /// <exception cref="JsonException">Thrown when JSON is invalid or cannot be deserialized.</exception>
         public static FilterParameter? FromJson(string json)
         {
             if (string.IsNullOrWhiteSpace(json))
@@ -58,13 +58,16 @@ namespace GpuImageProcessing.Core.Models
         }
 
         /// <summary>
-        /// Attempts to deserialize a FilterParameter from a JSON string
+        /// Attempts to deserialize a FilterParameter from a JSON string.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize</param>
-        /// <param name="value">Receives the deserialized FilterParameter if successful</param>
-        /// <returns>True if deserialization succeeded; otherwise, false</returns>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized FilterParameter if successful.</param>
+        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
         public static bool TryFromJson(string json, out FilterParameter? value)
         {
+            ArgumentNullException.ThrowIfNull(json);
+
             value = default;
 
             if (string.IsNullOrWhiteSpace(json))
