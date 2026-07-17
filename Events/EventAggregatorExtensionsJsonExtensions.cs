de98769bf86043f93be3e12171a0c9d3886a35c5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:56:58 +0300

    Readability pass over EventAggregatorExtensionsJsonExtensions

--- Events/EventAggregatorExtensionsJsonExtensions.cs
+++ Events/EventAggregatorExtensionsJsonExtensions.cs
@@ -12,8 +12,7 @@ using System.Text.Json.Serialization;
 namespace GpuImageProcessing.Events
 {
     /// <summary>
-    /// Provides System.Text.Json serialization extensions for <see cref="EventAggregator"/> instances
-    /// that use the EventAggregatorExtensions extension methods.
+    /// Provides System.Text.Json serialization extensions for <see cref="EventAggregator"/> instances.
     /// </summary>
     public static class EventAggregatorExtensionsJsonExtensions
     {
@@ -26,12 +25,12 @@ namespace GpuImageProcessing.Events
         };
 
         /// <summary>
-        /// Serializes the EventAggregator instance to a JSON string
+        /// Serializes the EventAggregator instance to a JSON string.
         /// </summary>
-        /// <param name="value">The event aggregator instance to serialize</param>
-        /// <param name="indented">Whether to format the JSON with indentation for readability</param>
-        /// <returns>JSON representation of the event aggregator instance</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+        /// <param name="value">The event aggregator instance to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+        /// <returns>JSON representation of the event aggregator instance.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static string ToJson(this EventAggregator value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -47,11 +46,11 @@ namespace GpuImageProcessing.Events
         }
 
         /// <summary>
-        /// Deserializes a JSON string to an EventAggregator instance
+        /// Deserializes a JSON string to an EventAggregator instance.
         /// </summary>
-        /// <param name="json">JSON string to deserialize</param>
-        /// <returns>Deserialized event aggregator instance, or null if JSON is null or empty</returns>
-        /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized</exception>
+        /// <param name="json">JSON string to deserialize.</param>
+        /// <returns>Deserialized event aggregator instance, or <see langword="null"/> if JSON is <see langword="null"/> or empty.</returns>
+        /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
         public static EventAggregator? FromJson(string json)
         {
             if (string.IsNullOrEmpty(json))
@@ -63,11 +62,11 @@ namespace GpuImageProcessing.Events
         }
 
         /// <summary>
-        /// Attempts to deserialize a JSON string to an EventAggregator instance
+        /// Attempts to deserialize a JSON string to an EventAggregator instance.
         /// </summary>
-        /// <param name="json">JSON string to deserialize</param>
-        /// <param name="value">Output parameter containing the deserialized instance if successful</param>
-        /// <returns>True if deserialization succeeded; otherwise, false</returns>
+        /// <param name="json">JSON string to deserialize.</param>
+        /// <param name="value">Output parameter containing the deserialized instance if successful.</param>
+        /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
         public static bool TryFromJson(string json, out EventAggregator? value)
         {
             value = null;
