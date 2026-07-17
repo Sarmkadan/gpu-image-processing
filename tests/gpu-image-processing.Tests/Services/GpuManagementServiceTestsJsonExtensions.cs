38341e192c0158be71f30ad4bcfb3fb399986bb0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tighten guard clauses in GpuManagementServiceTestsJsonExtensions

--- tests/gpu-image-processing.Tests/Services/GpuManagementServiceTestsJsonExtensions.cs
+++ tests/gpu-image-processing.Tests/Services/GpuManagementServiceTestsJsonExtensions.cs
@@ -9,68 +9,72 @@ namespace GpuImageProcessing.Tests.Services;
 /// </summary>
 public static class GpuManagementServiceTestsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Serializes the <see cref="GpuManagementServiceTests"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The value to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation.</param>
-    /// <returns>A JSON string representation of the value.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
-    public static string ToJson(this GpuManagementServiceTests value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the <see cref="GpuManagementServiceTests"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The value to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation.</param>
+	/// <returns>A JSON string representation of the value.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+	public static string ToJson(this GpuManagementServiceTests value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		return JsonSerializer.Serialize(value, CreateOptions(indented));
+	}
 
-        return JsonSerializer.Serialize(value, options);
-    }
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="GpuManagementServiceTests"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized instance, or <see langword="null"/> if the JSON represents a null value.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static GpuManagementServiceTests? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="GpuManagementServiceTests"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized instance, or <see langword="null"/> if the JSON represents a null value.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static GpuManagementServiceTests? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+		return JsonSerializer.Deserialize<GpuManagementServiceTests>(json, _jsonOptions);
+	}
 
-        return JsonSerializer.Deserialize<GpuManagementServiceTests>(json, _jsonOptions);
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="GpuManagementServiceTests"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized instance, or <see langword="null"/> if the JSON represents a null value.</param>
+	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	public static bool TryFromJson(string json, out GpuManagementServiceTests? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="GpuManagementServiceTests"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized instance, or <see langword="null"/> if the JSON represents a null value.</param>
-    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
-    public static bool TryFromJson(string json, out GpuManagementServiceTests? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+		try
+		{
+			value = JsonSerializer.Deserialize<GpuManagementServiceTests>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 
-        try
-        {
-            value = JsonSerializer.Deserialize<GpuManagementServiceTests>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+	/// <summary>
+	/// Creates a <see cref="JsonSerializerOptions"/> instance with the specified indentation setting.
+	/// </summary>
+	/// <param name="indented">Whether to format the JSON with indentation.</param>
+	/// <returns>A configured <see cref="JsonSerializerOptions"/> instance.</returns>
+	private static JsonSerializerOptions CreateOptions(bool indented)
+		=> new(_jsonOptions)
+		{
+			WriteIndented = indented
+		};
 }
