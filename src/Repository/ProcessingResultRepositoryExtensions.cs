4ff177dacb4b52ac429643720afdd498729d894b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    dedupe some logic in ProcessingResultRepositoryExtensions

--- src/Repository/ProcessingResultRepositoryExtensions.cs
+++ src/Repository/ProcessingResultRepositoryExtensions.cs
@@ -2,8 +2,9 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
+using System.Linq;
 using GpuImageProcessing.Domain;
 using GpuImageProcessing.Core;
 
@@ -22,7 +23,7 @@ public static class ProcessingResultRepositoryExtensions
     /// <param name="imageIds">Collection of image IDs to filter by</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Collection of processing results matching the specified image IDs</returns>
-    /// <exception cref="ArgumentNullException">Thrown when imageIds is null</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="repository"/> or <paramref name="imageIds"/> is null</exception>
     public static async Task<IReadOnlyList<ProcessingResult>> GetByImageIdsAsync(
         this ProcessingResultRepository repository,
         IEnumerable<Guid> imageIds,
@@ -31,13 +32,13 @@ public static class ProcessingResultRepositoryExtensions
         ArgumentNullException.ThrowIfNull(repository);
         ArgumentNullException.ThrowIfNull(imageIds);
 
-        var results = new List<ProcessingResult>();
-        foreach (var imageId in imageIds)
-        {
-            var batchResults = await repository.GetByImageIdAsync(imageId, cancellationToken);
-            results.AddRange(batchResults);
-        }
+        if (!imageIds.Any())
+            return Array.Empty<ProcessingResult>();
 
+        var tasks = imageIds.Select(imageId => repository.GetByImageIdAsync(imageId, cancellationToken));
+        var batchResults = await Task.WhenAll(tasks);
+
+        var results = batchResults.SelectMany(batch => batch).ToList();
         return results.AsReadOnly();
     }
 
@@ -48,7 +49,7 @@ public static class ProcessingResultRepositoryExtensions
     /// <param name="statuses">Collection of statuses to filter by</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Collection of processing results matching any of the specified statuses</returns>
-    /// <exception cref="ArgumentNullException">Thrown when statuses is null</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="repository"/> or <paramref name="statuses"/> is null</exception>
     public static async Task<IReadOnlyList<ProcessingResult>> GetByStatusesAsync(
         this ProcessingResultRepository repository,
         IEnumerable<ProcessingStatus> statuses,
@@ -57,13 +58,13 @@ public static class ProcessingResultRepositoryExtensions
         ArgumentNullException.ThrowIfNull(repository);
         ArgumentNullException.ThrowIfNull(statuses);
 
-        var results = new List<ProcessingResult>();
-        foreach (var status in statuses)
-        {
-            var batchResults = await repository.GetByStatusAsync(status, cancellationToken);
-            results.AddRange(batchResults);
-        }
+        if (!statuses.Any())
+            return Array.Empty<ProcessingResult>();
 
+        var tasks = statuses.Select(status => repository.GetByStatusAsync(status, cancellationToken));
+        var batchResults = await Task.WhenAll(tasks);
+
+        var results = batchResults.SelectMany(batch => batch).ToList();
         return results.AsReadOnly();
     }
 
@@ -71,10 +72,11 @@ public static class ProcessingResultRepositoryExtensions
     /// Gets processing results completed within the last N days.
     /// </summary>
     /// <param name="repository">The repository instance</param>
-    /// <param name="days">Number of days to look back</param>
+    /// <param name="days">Number of days to look back. Must be greater than 0.</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Collection of processing results completed within the specified time window</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when days is less than 1</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="repository"/> is null</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="days"/> is less than 1</exception>
     public static async Task<IReadOnlyList<ProcessingResult>> GetRecentlyCompletedAsync(
         this ProcessingResultRepository repository,
         int days,
@@ -82,7 +84,7 @@ public static class ProcessingResultRepositoryExtensions
     {
         ArgumentNullException.ThrowIfNull(repository);
         if (days < 1)
-            throw new ArgumentOutOfRangeException(nameof(days), "Days must be greater than 0");
+            throw new ArgumentOutOfRangeException(nameof(days), days, "Days must be greater than 0");
 
         var endDate = DateTime.UtcNow;
         var startDate = endDate.AddDays(-days);
@@ -95,11 +97,12 @@ public static class ProcessingResultRepositoryExtensions
     /// Gets processing results with the longest processing times.
     /// </summary>
     /// <param name="repository">The repository instance</param>
-    /// <param name="count">Maximum number of results to return</param>
-    /// <param name="minimumProcessingTimeMs">Minimum processing time threshold in milliseconds</param>
+    /// <param name="count">Maximum number of results to return. Default is 10.</param>
+    /// <param name="minimumProcessingTimeMs">Minimum processing time threshold in milliseconds. Only results with processing time greater than or equal to this value will be returned.</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Collection of processing results sorted by processing time (descending)</returns>
-    /// <exception cref="ArgumentOutOfRangeException">Thrown when count is less than 1 or minimumProcessingTimeMs is negative</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="repository"/> is null</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="count"/> is less than 1 or <paramref name="minimumProcessingTimeMs"/> is negative</exception>
     public static async Task<IReadOnlyList<ProcessingResult>> GetLongestRunningResultsAsync(
         this ProcessingResultRepository repository,
         int count = 10,
@@ -108,9 +111,9 @@ public static class ProcessingResultRepositoryExtensions
     {
         ArgumentNullException.ThrowIfNull(repository);
         if (count < 1)
-            throw new ArgumentOutOfRangeException(nameof(count), "Count must be greater than 0");
+            throw new ArgumentOutOfRangeException(nameof(count), count, "Count must be greater than 0");
         if (minimumProcessingTimeMs < 0)
-            throw new ArgumentOutOfRangeException(nameof(minimumProcessingTimeMs), "Minimum processing time cannot be negative");
+            throw new ArgumentOutOfRangeException(nameof(minimumProcessingTimeMs), minimumProcessingTimeMs, "Minimum processing time cannot be negative");
 
         var allResults = await repository.GetAllAsync(cancellationToken);
         var filteredResults = allResults
