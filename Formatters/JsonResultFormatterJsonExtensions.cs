92811828389a37d34b6dda8953911f0b9de56a75
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    polish JsonResultFormatterJsonExtensions.

--- Formatters/JsonResultFormatterJsonExtensions.cs
+++ Formatters/JsonResultFormatterJsonExtensions.cs
@@ -16,11 +16,14 @@ namespace GpuImageProcessing.Formatters
     /// </summary>
     public static class JsonResultFormatterJsonExtensions
     {
+        /// <summary>
+        /// Gets the JSON serializer options used for serializing and deserializing <see cref="JsonResultFormatter"/> instances.
+        /// </summary>
         private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerOptions.Default)
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             WriteIndented = false,
-            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
         };
 
         static JsonResultFormatterJsonExtensions()
@@ -35,7 +38,7 @@ namespace GpuImageProcessing.Formatters
         /// <param name="value">The formatter instance to serialize.</param>
         /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
         /// <returns>A JSON string representation of the formatter.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
         public static string ToJson(this JsonResultFormatter value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -52,8 +55,8 @@ namespace GpuImageProcessing.Formatters
         /// Deserializes a JSON string to a <see cref="JsonResultFormatter"/> instance.
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
-        /// <returns>The deserialized formatter instance, or null if the JSON is invalid.</returns>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+        /// <returns>The deserialized formatter instance, or <see langword="null"/> if the JSON is invalid.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
         public static JsonResultFormatter? FromJson(string json)
         {
             ArgumentException.ThrowIfNullOrEmpty(json);
@@ -73,8 +76,8 @@ namespace GpuImageProcessing.Formatters
         /// </summary>
         /// <param name="json">The JSON string to deserialize.</param>
         /// <param name="value">Receives the deserialized formatter instance if successful.</param>
-        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+        /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
         public static bool TryFromJson(string json, out JsonResultFormatter? value)
         {
             ArgumentException.ThrowIfNullOrEmpty(json);
