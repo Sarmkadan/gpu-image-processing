95c1e74382026b5e30a1a5bcc1135eada3e4f91b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 08:03:02 +0300

    get PerformanceMetricsExtensionsTests compiilng again

---
+++ tests/gpu-image-processing.Tests/PerformanceMetricsExtensionsTests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace gpu_image_processing.Tests
+{
+    public class PerformanceMetricsExtensionsTests
+    {
+        [Fact]
+        public async Task CalculateAverageExecutionTime_ValidInput_ReturnsPositiveValue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task CalculateAverageExecutionTime_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task IsGpuUnderutilized_ValidInput_ReturnsTrue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task IsGpuUnderutilized_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task HasThroughputDropped_ValidInput_ReturnsTrue()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task HasThroughputDropped_NullInput_ThrowsArgumentNullException()
+        {
+            // Test implementation
+        }
+    }
+}
