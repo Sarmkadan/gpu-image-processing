a9f8e937162bb4dc8b14ec241d540cde1b206053
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-14 23:30:44 +0000

    chore: maintenance updates

--- Integration/MetricsPublisherStatsExtensions.cs
+++ Integration/MetricsPublisherStatsExtensions.cs
@@ -9,16 +9,32 @@ namespace GpuImageProcessing.Integration
 {
     public class MetricsPublisherStatsExtensions
     {
+        /// <summary>
+        /// Initializes a new instance of the MetricsPublisherStatsExtensions class.
+        /// </summary>
+        /// <param name="publisher">The metrics publisher to extend.</param>
         public MetricsPublisherStatsExtensions(MetricsPublisher publisher)
         {
             // Initialize
         }
 
+        /// <summary>
+        /// Formats the metric as a Prometheus exposition line.
+        /// </summary>
+        /// <param name="name">The name of the metric.</param>
+        /// <param name="value">The value of the metric.</param>
+        /// <param name="tags">The tags associated with the metric.</param>
         public void FormatMetricAsPrometheusLine(string name, double value, Dictionary<string, string> tags)
         {
             // Implement formatting as a Prometheus exposition line
         }
 
+        /// <summary>
+        /// Validates and clamps the metric value.
+        /// </summary>
+        /// <param name="name">The name of the metric.</param>
+        /// <param name="value">The value of the metric.</param>
+        /// <param name="tags">The tags associated with the metric.</param>
         public void ValidateMetricValue(string name, double value, Dictionary<string, string> tags)
         {
             // Implement validation/clamping of metric values
